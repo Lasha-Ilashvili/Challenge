d0a9e1cd5b931ae6a1e77ec6ecba7bf270b1f3ca
Lasha-Ilashvili <148916134+Lasha-Ilashvili@users.noreply.github.com>
2024-01-10 12:22:15 +0400

    Delete app/build directory

--- app/build/tmp/kapt3/stubs/debug/com/example/challenge/domain/repository/connection/ConnectionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.challenge.domain.repository.connection;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\bf\u0018\u00002\u00020\u0001J#\u0010\u0002\u001a\u0014\u0012\u0010\u0012\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00060\u00050\u00040\u0003H\u00a6@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0007\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\b"}, d2 = {"Lcom/example/challenge/domain/repository/connection/ConnectionsRepository;", "", "getConnections", "Lkotlinx/coroutines/flow/Flow;", "Lcom/example/challenge/data/common/Resource;", "", "Lcom/example/challenge/domain/model/connection/GetConnection;", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
-public abstract interface ConnectionsRepository {
-    
-    @org.jetbrains.annotations.Nullable
-    public abstract java.lang.Object getConnections(@org.jetbrains.annotations.NotNull
-    kotlin.coroutines.Continuation<? super kotlinx.coroutines.flow.Flow<? extends com.example.challenge.data.common.Resource<? extends java.util.List<com.example.challenge.domain.model.connection.GetConnection>>>> $completion);
-}
